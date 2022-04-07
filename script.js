dbbbc56fb655b26a1cdd416b3d018c554e208d87
RamunnoAJ <ramunnoagustin@gmail.com>
2022-04-07 01:18:48 -0300

    master

---
+++ script.js
@@ -0,0 +1,38 @@
+document.querySelector('#push').onclick = function () {
+    // ALERT TO FILL THE NEW TASK
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("Please Enter a Task")
+    }
+    // ADD NEW TASK
+    else {
+        document.querySelector('#tasks').innerHTML
+            += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                    </span>
+                    <button class="delete">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+            </div>
+            `;
+        
+        // DELETE TASK
+        let current_tasks = document.querySelectorAll(".delete");
+        for (let i = 0; i < current_tasks.length; i++){
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+        // CROSSING OFF A COMPLETED TASK
+        let tasks = document.querySelectorAll(".task");
+        for (let i = 0; i < tasks.length; i++){
+            tasks[i].onclick = function () {
+                this.classList.toggle('completed');
+            }
+        }
+
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
